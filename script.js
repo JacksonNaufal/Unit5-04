d715880e8bf8bbe9cdd5b641718ef84e520c0083
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-17 15:19:45 +0000

    minor changes

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ function picturechange () {
   document.getElementById('paragraph').style.color = "magenta"
   document.getElementById('picture').src = 
   'Aventador.jpg'
-}
-
-function picturechange () {
-  document.getElementById('picture').src = 
+   document.getElementById('picture').src = 
   'Lamborghini Huracan.jpg'
   document.getElementById('paragraph').innerHTML =
   'This is a Lamborghini Huracan'
 }
+
+
+
 
 function switchback () {
   document.getElementById('picture').src =
