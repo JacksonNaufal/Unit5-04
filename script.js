fa291f514357e7a9812d4c2746ad40c5dd4c418c
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-17 16:21:41 +0000

    added explanation

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.getElementById('switchback').addEventListener('click', switchback)
 function background () {
   document.getElementById('body').style.backgroundColor = 'pink'
 }
+// This function changes the body, because the body in the HTML is named body, and the event listner on line 1 finds where the ids are, on the HTML and it branches the together
 
 function picturechange () {
   document.getElementById('paragraph').style.color = 'magenta'
@@ -16,9 +17,13 @@ function picturechange () {
   'This is a Lamborghini Huracan'
 }
 
+// This function connects to line 2 which basically, tells the event listner to locate the ids, which correlate with the ', and to also replace them with the innerHTML or the src, after it figures out what to replace, and where to replace it.
+
 function switchback () {
   document.getElementById('picture').src =
   'CENTENARIO.jpg'
   document.getElementById('paragraph').innerHTML =
   'Its back to the Centenario!'
 }
+
+// This basically uses the same pictures, where it knows that to replace, and it knows to replace it with the ids labeled paragrapj and  picutes, due to the eventlistner on line 3, finding everything
